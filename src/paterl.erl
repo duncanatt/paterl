a8ab4d7d53a69e0e048f4c18ef94404cc7c69ade
Duncan Paul Attard <duncanatt@gmail.com>
2024-04-29 18:01:48 +0100

    Added callout to Pat in external process and integrated error parsing in 'paterl.erl' module.

--- src/paterl.erl
+++ src/paterl.erl
@@ -14,6 +14,24 @@
 
 %% API
 -export([compile/2]).
+-compile(export_all).
+
+%%% ----------------------------------------------------------------------------
+%%% Macro and record definitions.
+%%% ----------------------------------------------------------------------------
+
+%%% Generic constants.
+
+%% Mailbox annotation primitive type.
+-define(EXEC, "/Users/duncan/Dropbox/Postdoc/Development/mbcheck/mbcheck").
+
+%%% Error types.
+
+%% Pat error with message.
+-define(E_PAT_MSG, e_pat_msg).
+
+%% Unknown Pat error.
+-define(E_PAT_NONE, e_pat_none).
 
 compile(File, Opts) when is_list(File), is_list(Opts) ->
   case epp:parse_file(File, Opts) of
@@ -28,10 +46,7 @@ compile(File, Opts) when is_list(File), is_list(Opts) ->
           % Get program types table.
           case paterl_types:table(Forms) of
             {ok, TInfo = #t_info{types = Types, specs = Specs, mb_defs = MbDefs, mb_names = MbNames}, Warnings1} ->
-              % Type table is valid.
-
-
-              % Table valid but possible warnings.
+              % Type table valid but possible warnings.
               errors:show_warnings({File, Warnings1}),
 
               io:format("~n~s TINFO ~s~n", [lists:duplicate(40, $-), lists:duplicate(40, $-)]),
@@ -41,10 +56,8 @@ compile(File, Opts) when is_list(File), is_list(Opts) ->
               io:format("MbNames: ~p~n", [MbNames]),
               io:format("~s SIGS & TINFO ~s~n", [lists:duplicate(40, $-), lists:duplicate(40, $-)]),
 
-
+              % Annotate forms TODO: Update comment.
               case paterl_anno:annotate(Forms, TInfo) of
-
-%%                {Annotated, Error2} = paterl_anno:annotate(Forms, TInfo),
 
                 {ok, Annotated} ->
                   io:format("~n~n~nOriginal forms:~n~p~n", [Forms]),
@@ -54,15 +67,35 @@ compile(File, Opts) when is_list(File), is_list(Opts) ->
                   compile:forms(Annotated),
 
                   Pat = paterl_trans:translate(Annotated),
-                  io:format("~n~n~nOutput Pat:~n~n~n~s~n", [Pat]);
+                  io:format("~n~n~nOutput Pat:~n~n~n~s~n", [Pat]),
+
+
+                  PatFile = filename:rootname(File),
+                  case file:write_file(PatFile, Pat) of
+                    ok ->
+                      case exec(?EXEC ++ " " ++ PatFile) of
+                        {0, _} ->
+                          % Generated Pat file type-checked successfully.
+                          ok;
+                        {_, Bytes} ->
+                          % Generated Pat file contains type errors.
+                          case re:split(lists:flatten(Bytes), "^\\[.*\\] (.*)$", [trim, {return, list}]) of
+                            [[]] ->
+                              % Pat error output empty.
+                              errors:show_error({?MODULE, ?E_PAT_NONE});
+                            [[] | Msg] ->
+                              % Defined Pat error.
+                              errors:show_error({?MODULE, {?E_PAT_MSG, Msg}})
+                          end
+                      end;
+                    {error, Error} -> % Reason
+                      errors:show_error({file, Error})
+                  end;
                 #error{errors = Errors2} ->
                   io:format("Errors found: ~p", [Errors2]),
                   % File contains mailbox annotation errors.
                   errors:show_errors({File, Errors2})
               end;
-
-
-%%              ok;
             #error{errors = Errors1, warnings = Warnings1} ->
               % File contains errors.
 %%              errors:show_warnings([{File, Warnings1}]),
@@ -71,7 +104,6 @@ compile(File, Opts) when is_list(File), is_list(Opts) ->
               errors:show_errors({File, Errors1}),
               errors
           end;
-
 
 
         {error, Errors, Warnings} ->
@@ -89,3 +121,44 @@ compile(File, Opts) when is_list(File), is_list(Opts) ->
       errors:show_error({epp, Error}),
       error
   end.
+
+%% @private Executes specified command on shell and returns the status and
+%% command output.
+%%
+%% Call is synchronous.
+-spec exec(string()) -> iolist().
+exec(Cmd) ->
+  Port = open_port(
+    {spawn, Cmd},
+    [stream, in, eof, hide, exit_status, stderr_to_stdout]
+  ),
+  get_output(Port, []).
+
+%% @private Retrieves the command output and closes the port.
+-spec get_output(port(), iolist()) -> iolist().
+get_output(Port, Read) ->
+  receive
+    {Port, {data, Bytes}} ->
+      get_output(Port, [Read | Bytes]);
+    {Port, eof} ->
+      port_close(Port),
+      Status =
+        receive
+          {Port, {exit_status, Code}} ->
+            Code
+        end,
+      {Status, Read}
+  end.
+
+%%% ----------------------------------------------------------------------------
+%%% Error handling and reporting.
+%%% ----------------------------------------------------------------------------
+
+%% @doc Formats the specified error to human-readable form.
+format_error({?E_PAT_MSG, Reason}) ->
+  io_lib:format(
+    "~s",
+    [Reason]
+  );
+format_error(?E_PAT_NONE) ->
+  "Unknown Pat error; see generated output file".
