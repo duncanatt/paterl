b6e849fe94511fb2654051e783d81130d5851b83
Duncan Paul Attard <duncanatt@gmail.com>
2024-07-22 11:18:53 +0100

    Improved checking of regexp in the 'pat_regex' module so that now it accounts for spaces.

--- src/pat/pat_regex.erl
+++ src/pat/pat_regex.erl
@@ -19,7 +19,8 @@
 
 %% @private Checks whether a mailbox can become potentially empty.
 is_mb_empty(Regex) ->
-  case re:run(Regex, "^(\\*.*|1.*|.*1)$") of
+%%  case re:run(Regex, "^(\\*.[A-Z][a-z]*|1\s*\\+.*|[A-Z][a-z]+1|[A-Z][a-z]+)$") of
+  case re:run(trim(Regex), "^(\\*.*|1.*|.*1)$") of
     {match, _} ->
       true;
     _ ->
@@ -29,3 +30,7 @@ is_mb_empty(Regex) ->
 % TODO: Requires a regular expression parser.
 simplify([]) ->
   ok.
+
+trim(Regex) ->
+  re:replace(Regex, "\s+", "", [global, {return, list}]).
+
