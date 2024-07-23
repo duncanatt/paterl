81a4c57921e8708444d387ce1776a099cca44e3b
Duncan Paul Attard <duncanatt@gmail.com>
2024-07-23 12:00:31 +0100

    Added future.

--- src/examples/erlang/de_liguoro_padovani/future.erl
+++ src/examples/erlang/de_liguoro_padovani/future.erl
@@ -2,12 +2,15 @@
 %%% @author duncan
 %%% @copyright (C) 2023, <COMPANY>
 %%% @doc
-%%%
+%%% A future that supports one write and multiple reads.
 %%% @end
-%%% Created : 21. Nov 2023 16:53
+%%% Created : 14. May 2024 18:02
 %%%-------------------------------------------------------------------
 -module(future).
 -author("duncan").
+
+%%% Includes.
+-include("paterl.hrl").
 
 %%% Imports.
 -import(io, [format/2]).
@@ -19,100 +22,88 @@
 -export([future/0]).
 
 
+%%% ----------------------------------------------------------------------------
 %%% Type definitions.
+%%% ----------------------------------------------------------------------------
 
-%% Future interface (a la EDoc annotations!).
-%% interface Future { Put(Int), Get(User!) }
-%% @type future() :: {put, integer()} | {get, user()} (don't need the pid if I'm not using dialyzer)
+%%% Messages.
 
-%% User interface.
-%% interface User { Reply(Int) }
-%% @type user() :: {reply, integer()} | (don't need the pid if I'm not using dialyzer)
+%% Future.
+-type put() :: {put, integer()}.
+-type get() :: {get, user_mb()}.
 
+%% User.
+-type reply() :: {reply, integer()}.
+
+%%% Interfaces.
+
+%% Future.
+-type future_mb() :: pid() | put() | get().
+
+%% User.
+-type user_mb() :: pid() | reply().
+
+%% Main.
+-type main_mb() :: pid().
+
+%%% Interface-function associations.
+
+%% Future.
+-new({future_mb, [future/0]}).
+-use({future_mb, [resolved_future/1]}).
+
+%% User.
+-new({user_mb, [user/1]}).
+
+%% Main.
+-new({main_mb, [main/0]}).
+
+%%% ----------------------------------------------------------------------------
 %%% API.
+%%% ----------------------------------------------------------------------------
 
-%% def future(self: Future?): Unit {
-%%   guard self : Put.(*Get) {
-%%     receive Put(x) from self -> resolvedFuture(self, x)
-%%   }
-%% }
-%% @spec future() -> none()
-%% @new future()
+%% @doc Initializes the future.
+-spec future() -> no_return().
 future() ->
-  %% @mb future()
-  %% @assert put.get*
+  ?mb_assert_regex("Put . *Get"),
   receive {put, Value} ->
-    %% @use future() (@use is derived from the interface of the resolved_future function)
     resolved_future(Value)
   end.
 
-%% def resolvedFuture(self: Future?, value: Int): Unit {
-%%   guard self : *Get {
-%%     free -> ()
-%%     receive Put(x) from self ->
-%%       fail(self)[Unit]
-%%     receive Get(user) from self ->
-%%       user ! Reply(value);
-%%       resolvedFuture(self, value)
-%%   }
-%% }
-%% @spec resolved_future(integer()) -> none()
-%% @use future()
+%% @doc Resolved future servicing requests.
+-spec resolved_future(integer()) -> no_return().
 resolved_future(Value) ->
-  %% @mb future()
-  %% @assert get*
+  ?mb_assert_regex("*Get"),
   receive
     {get, UserPid} ->
       UserPid ! {reply, Value},
-      %% @use future()
       resolved_future(Value)
   end.
 
-%% def user(future: Future!): Int {
-%%   let self = new[User] in
-%%   future ! Get(self);
-%%   guard self : Reply {
-%%     receive Reply(x) from self ->
-%%     free(self);
-%%     x
-%%   }
-%% }
-%% @spec user(future()) -> integer()
-%% @new user()
+%% @doc User requesting the value held in the future.
+-spec user(future_mb()) -> integer().
 user(FuturePid) ->
-  Self =
-    %% @mb user()
-    self(),
+  Self = self(),
   FuturePid ! {get, Self},
 
-  %% @mb user()
-  %% @assert reply
+  ?mb_assert_regex("Reply"),
   receive
     {reply, Value} ->
       Value
   end.
 
-%% def main(): Unit {
-%%   let future_mb = new[Future] in
-%%   spawn { future(future_mb) };
-%%   future_mb ! Put(5);
-%%   future_mb ! Put(5);
-%%   print(intToString(user(future_mb)));
-%%   print(intToString(user(future_mb)))
-%% }
-%% @spec  main() -> any()
-%% @new user()
+%% @doc Launcher.
+-spec main() -> any().
 main() ->
-  FuturePid =
-    %% @new future()
-    spawn(?MODULE, future, []),
+  ?mb_new(future_mb),
+  FuturePid = spawn(?MODULE, future, []),
   FuturePid ! {put, 5},
 
-  Get1 =
-    %% @new user
-    user(FuturePid),
+  Get1 = user(FuturePid),
   format("A: ~p~n", [Get1]),
-  Get2 =
-    %% @new user
-    user(FuturePid),
+
+  Get2 = user(FuturePid),
   format("B: ~p~n", [Get2]).
+
+
+%% ./src/paterl src/examples/erlang/de_liguoro_padovani/future.erl -v all -I include
