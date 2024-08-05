6e428bc4566787c3db20e58ea512ffd0976f4704
Duncan Paul Attard <duncanatt@gmail.com>
2024-08-05 16:56:22 +0100

    Finished 'master_worker' module.

--- src/examples/erlang/de_liguoro_padovani/master_worker.erl
+++ src/examples/erlang/de_liguoro_padovani/master_worker.erl
@@ -2,114 +2,108 @@
 %%% @author walker
 %%% @copyright (C) 2024, <COMPANY>
 %%% @doc
-%%%
+%%% Master-worker set-up.
 %%% @end
-%%% Created : 05. 7月 2024 10:26
+%%% Created : 14. May 2024 18:02
 %%%-------------------------------------------------------------------
 -module(master_worker).
--author("walker").
+-author("duncan").
 
+%%% Includes.
 -include("paterl.hrl").
 
+%%% Imports.
 -import(io, [format/2]).
 
-%% API
+%%% API.
 -export([main/0]).
 
-%% Internal exports
--export([master/0, worker/0, farm/3, harvest/3, client/2]).
+%%% Internal exports.
+-export([master/0, worker/0, client/2]).
 
-%% Mailbox interface-function associations
+
+%%% ----------------------------------------------------------------------------
+%%% Type definitions.
+%%% ----------------------------------------------------------------------------
+
+%%% Messages.
+
+%% Master.
+-type task() :: {task, client_mb(), integer()}.
+
+%% Pool.
+-type result() :: {result, integer()}.
+
+%% Worker.
+-type work() :: {work, pool_mb(), integer()}.
+
+%%% Interfaces.
+
+%% Master.
+-type master_mb() :: pid() | task().
+
+%% Pool.
+-type pool_mb() :: pid() | result().
+
+%% Worker.
+-type worker_mb() :: pid() | work().
+
+%% Client.
+-type client_mb() :: pid() | result().
+
+%% Main.
+-type main_mb() :: pid().
+
+%%% Interface-function associations.
+
+%% Master.
 -new({master_mb, [master/0]}).
 -use({master_mb, [master_loop/0]}).
 
+%% Pool.
 -new({pool_mb, [pool/1]}).
 -use({pool_mb, [harvest/3, harvest_exit/0]}).
 
-
+%% Worker.
 -new({worker_mb, [worker/0]}).
 
-
+%% Client.
 -new({client_mb, [client/2]}).
+
+%% Main.
 -new({main_mb, [main/0]}).
 
-%% MasterMb's message types
--type task() :: {task, client_mb(), integer()}.
 
-%% PoolMb's message types
--type result() :: {result, integer()}.
+%%% ----------------------------------------------------------------------------
+%%% API.
+%%% ----------------------------------------------------------------------------
 
-%% WorkerMb's message types
--type work() :: {work, pool_mb(), integer()}.
-
-%% # Mailbox message interfaces.
-%% interface MasterMb {
-%%    Task(ClientMb!, Int)
-%% }
-%%
-%% interface PoolMb {
-%%    Result(Int)
-%% }
-%%
-%% interface WorkerMb {
-%%    Work(PoolMb!, Int)
-%% }
-%%
-%% interface ClientMb {
-%%    Result(Int)
-%% }
--type master_mb() :: pid() | task().
--type pool_mb() :: pid() | result().
--type worker_mb() :: pid() | work().
--type client_mb() :: pid() | result().
--type main_mb() :: pid().
-
-%% def master(self: MasterMb?): Unit {
-%%   guard self: *Task {
-%%     free -> () # No more tasks to handle.
-%%     receive Task(replyTo, n) from self ->
-%%
-%%       let pool = new[PoolMb] in
-%%
-%%       farm(0, n, pool);
-%%
-%%       let result = harvest(0, pool) in
-%%       replyTo ! Result(result);
-%%
-%%       master(self)
-%%   }
-%% }
+%% @doc Master server loop handling incoming client tasks.
 -spec master() -> no_return().
 master() ->
   master_loop().
 
+%% @doc Master server loop handling incoming client tasks.
 -spec master_loop() -> no_return().
 master_loop() ->
   ?mb_assert_regex("*Task"),
   receive
     {task, ReplyTo, N} ->
+      format("Received task to compute ~b from client ~p.~n", [N, ReplyTo]),
       Result = pool(N),
+      format("Received result ~b from workers.~n", [Result]),
       ReplyTo ! {result, Result},
       master_loop()
   end.
 
-% New
+%% @doc Pool that interfaces with workers to farm tasks and harvest results.
 -spec pool(integer()) -> integer().
 pool(Chunks) ->
   Self = self(),
   farm(0, Chunks, Self),
-%%  Self = self(),
   harvest(0, Chunks, 0).
 
-
-
-%% def worker(self: WorkerMb?): Unit {
-%%   guard self: Work {
-%%     receive Work(replyTo, n) from self ->
-%%       replyTo ! Result(compute(n));
-%%       free(self)
-%%   }
-%% }
+%% @doc Worker computing assigned task by master.
 -spec worker() -> no_return().
 worker() ->
   ?mb_assert_regex("Work"),
@@ -119,20 +113,7 @@ worker() ->
       ReplyTo ! {result, Result}
   end.
 
-%% def farm(count: Int, chunks: Int, pool: PoolMb!): Unit {
-%%   if (count == chunks) {
-%%     ()
-%%   }
-%%   else {
-%%     let task = count + 1 in
-%%
-%%     let workerMb = new[WorkerMb] in
-%%     spawn { worker(workerMb) };
-%%     workerMb ! Work(pool, task);
-%%
-%%     farm(task, chunks, pool)
-%%   }
-%% }
+%% @doc Distributes tasks between worker processes.
 -spec farm(integer(), integer(), pool_mb()) -> no_return().
 farm(Count, Chunks, Pool) ->
   if Count == Chunks ->
@@ -140,87 +121,73 @@ farm(Count, Chunks, Pool) ->
     true ->
       Task = Count + 1,
       ?mb_new(worker_mb),
-      WorkerMb = spawn(?MODULE, worker, []),
-      WorkerMb ! {work, Pool, Task},
+      Worker = spawn(?MODULE, worker, []),
+      Worker ! {work, Pool, Task},
+
+      format("Farmed chunk ~b to worker ~p.~n", [Task, Worker]),
       farm(Task, Chunks, Pool)
   end.
 
-%% def harvest(acc: Int, pool: PoolMb?): Int {
-%%   guard pool: *Result {
-%%     free ->
-%%       acc
-%%     receive Result(n) from pool ->
-%%       harvest(acc + n, pool)
-%%   }
-%% }
+%% @doc Collects and sums the individual results of the tasks assigned to
+%% workers.
 -spec harvest(integer(), integer(), integer()) -> integer().
 harvest(Count, Chunks, Acc) ->
+  format("Count ~p and chunks ~p~n", [Count, Chunks]),
   if (Count == Chunks) ->
     harvest_exit(), % To consume the possibly unconsumed Result messages and balance out the mailbox.
     % Note that this matches the pattern in the other branch, which seems to be key, in this simple case.
     % What about having something like ?garbage_collect("*Result") which generates the draining
-    % function automatically in Pat?
+    % function automatically in Pat but does not BLOCK the process forever in Erlang?
     Acc;
   true ->
     ?mb_assert_regex("*Result"),
     receive
       {result, Result} ->
         Count0 = Count + 1,
+
+        format("Harvested chunk ~b with result ~b.~n", [Count0, Result]),
         harvest(Count0, Chunks, Acc + Result)
     end
   end.
 
--spec harvest_exit() -> no_return().
+%% @doc Flushes the master mailbox.
+-spec harvest_exit() -> any().
 harvest_exit() ->
+  % TODO: This mailbox flushing function means that the process remains blocked forever.
   ?mb_assert_regex("*Result"),
   receive
     {result, Result} ->
       harvest_exit()
+  after 0 ->
+    ok
   end.
 
-%% def compute(n: Int): Int {
-%%    n * n
-%% }
+%% @doc Models a complex computation that a worker performs.
 -spec compute(integer()) -> integer().
 compute(N) ->
   N * N.
 
-%% def client(n: Int, self: ClientMb?, masterMb: MasterMb!): Unit {
-%%   masterMb ! Task(self, n);
-%%   guard(self) : Result {
-%%     receive Result(result) from self ->
-%%       free(self);
-%%       print(intToString(result))
-%%   }
-%% }
--spec client(integer(), master_mb()) -> no_return().
-client(N, MasterMb) ->
+%% @doc Client issuing one (numerical) task to the master.
+-spec client(integer(), master_mb()) -> any().
+client(N, Master) ->
   Self = self(),
-  MasterMb ! {task, Self, N},
+  Master ! {task, Self, N},
+
+  format("Client ~p sent task ~b to master ~p.~n", [self(), N, Master]),
   ?mb_assert_regex("Result"),
   receive
     {result, Result} ->
-      format("~p~n", [Result])
+      format("Result from master: ~b.~n", [Result])
   end.
 
-%% def main(): Unit {
-%%   let masterMb = new[MasterMb] in
-%%   spawn { master(masterMb) };
-%%
-%%   let client1 = new[ClientMb] in
-%%   spawn { client(5, client1, masterMb) };
-%%
-%%   let client2 = new[ClientMb] in
-%%   spawn { client(10, client2, masterMb) }
-%% }
+%% @doc Launcher.
 -spec main() -> any().
 main() ->
   ?mb_new(master_mb),
   MasterMb = spawn(?MODULE, master, []),
 
   ?mb_new(client_mb),
-  Client1 = spawn(?MODULE, client, [5, MasterMb]),
-
-  ?mb_new(client_mb),
-  Client2 = spawn(?MODULE, client, [10, MasterMb]),
+  spawn(?MODULE, client, [5, MasterMb]),
   ok.
+
+%% ./src/paterl src/examples/erlang/de_liguoro_padovani/master_worker.erl -v all -I include
