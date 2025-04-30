0fb48bbefd5dda0c809023172ff0df866f4c0c22
Duncan Paul Attard <duncanatt@gmail.com>
2025-04-30 14:11:59 +0200

    Updated paterl execution command for example.

--- src/examples/erlang/codebeam/id_ts_server_mul_mb.erl
+++ src/examples/erlang/codebeam/id_ts_server_mul_mb.erl
@@ -143,4 +143,4 @@ main() ->
   format("Id: ~p~n", [Id]).
 
 
-%% ./src/paterl src/examples/erlang/codebeam/id_ts_server_why_mul_mb_dont_work.erl -v all -I include
+%% ./src/paterl src/examples/erlang/codebeam/id_ts_server_mul_mb.erl -v all -I include
